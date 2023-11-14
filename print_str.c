f77a533149566954e1a406ff453ec2541c3735d3
akoredera <akoredera@gmail.com>
2023-11-14 10:31:12 -0800

    add: print_str file

---
+++ print_str.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_str - print string.
+ *
+ * @my_args: argument.
+ *
+ * Return: character count.
+ */
+int print_str(va_list my_args)
+{
+	int i, count = 0;
+	char *str = va_arg(my_args, char *);
+
+	if (!str)
+	{
+		str = "(null)";
+	}
+	if (str[0] == '\0')
+		return (-1);
+	for (i = 0; str[i] != '\0'; i++)
+		count = putchar(str[i]);
+	return (count);
+}
